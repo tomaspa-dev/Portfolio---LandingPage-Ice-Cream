afc2b63b95695fe550dcfda460ed39cb682e9d61
tomaspa-dev <blord.dev@gmail.com>
2024-01-17 17:00:31 -0500

    Global Changes
    Updating media queries

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-import {
-  annotate,
-  annotationGroup,
-} from "https://unpkg.com/rough-notation?module";
+// import {
+//   annotate,
+//   annotationGroup,
+// } from "https://unpkg.com/rough-notation?module";
 
 ///////////////////////////////////////////////////////////
 // highlight-heading
 ///////////////////////////////////////////////////////////
-const n1 = document.querySelector("#highlight-heading");
-const n2 = document.querySelector("#underline-heading");
-const a1 = annotate(n1, {
-  type: "highlight",
-  color: "#fbbf24",
-  padding: 5,
-  animation: false,
-});
-const a2 = annotate(n2, {
-  type: "underline",
-  color: "#fbbf24",
-  padding: 0,
-  strokeWidth: 3.5,
-  animate: false,
-});
-// a1.show();
+// const n1 = document.querySelector("#highlight-heading");
+// const n2 = document.querySelector("#underline-heading");
+// const a1 = annotate(n1, {
+//   type: "highlight",
+//   color: "#fbbf24",
+//   padding: 5,
+//   animation: false,
+// });
+// const a2 = annotate(n2, {
+//   type: "underline",
+//   color: "#fbbf24",
+//   padding: 0,
+//   strokeWidth: 3.5,
+//   animate: false,
+// });
+// // a1.show();
 
-const group = annotationGroup([a1, a2]);
-group.show();
+// const group = annotationGroup([a1, a2]);
+// group.show();
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
